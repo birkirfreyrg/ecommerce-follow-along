aff129f5f4f78c697bac6ca61c4c011ab92a982e
birkirfreyrg <birkirfrey@msn.com>
2023-08-08 15:21:20 +0000

    Added a Logo component, made Admin mobile responsive. Styles work in progress

--- pages/products.js
+++ pages/products.js
@@ -13,10 +13,7 @@ export default function Products() {
 
   return (
     <Layout>
-      <Link
-        className="bg-blue-900 text-white rounded-md py-1 px-2"
-        href={"/products/new"}
-      >
+      <Link className="btn-primary" href={"/products/new"}>
         Add new Product
       </Link>
       <table className="basic mt-2">
