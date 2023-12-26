2f4eec5dd62e09a947f655ae3ca869fdda7f5c1f
birkirfreyrg <birkirfrey@msn.com>
2023-12-26 10:35:43 +0000

    Fixed dates and confirmation of order paid.

--- pages/orders.js
+++ pages/orders.js
@@ -18,6 +18,7 @@ export default function OrdersPage() {
         <thead>
           <tr>
             <th>Date</th>
+            <th>Paid</th>
             <th>Rescipient</th>
             <th>Products</th>
           </tr>
@@ -26,7 +27,10 @@ export default function OrdersPage() {
           {orders.length > 0 &&
             orders.map((order) => (
               <tr>
-                <td>{order.createdAt}</td>
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td className={order.paid ? "text-green-600" : "text-red-600"}>
+                  {order.paid ? "YES" : "NO"}
+                </td>
                 <td>
                   {order.name} {order.email}
                   <br />
